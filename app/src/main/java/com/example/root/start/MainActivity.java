541698d722f2d4606695fe62f42c0fbcdb7f4c1e
bunty <hamzasafdar01@gmail.com>
2018-11-01 20:52:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/root/start/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.root.start;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            public void run() {
+                // TODO Auto-generated method stub
+                finish();
+                Intent menu = new Intent(getBaseContext(), login.class);
+                startActivity(menu);
+            }
+        }, 3000);
+
+
+    }
+}
